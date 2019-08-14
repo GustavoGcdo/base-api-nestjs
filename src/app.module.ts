f3859b61f56970a80aaca03574d6342ff9abf975
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2019-08-14 11:30:51 -0400

    Adicionado core module do app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [CoreModule],
   controllers: [],
   providers: [],
 })
