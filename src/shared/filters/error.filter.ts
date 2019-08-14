7a607c6a8537baeabccca8a850cf1b28a1d0e819
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2019-08-14 12:08:34 -0400

    Capturando internal server error

--- src/shared/filters/error.filter.ts
+++ src/shared/filters/error.filter.ts
@@ -1,30 +1,42 @@
-import { Catch, HttpException, ExceptionFilter, ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 import { Report } from '../notifiable/report';
 import { Result } from '../result/result';
 
-@Catch(HttpException)
+@Catch()
 export class ErrorFilter implements ExceptionFilter {
-    catch(exception: any, host: ArgumentsHost) {
-        const responseOfRequest = host.switchToHttp().getResponse();
-        const { response } = exception;
+  catch(exception: any, host: ArgumentsHost) {
+    const responseOfRequest = host.switchToHttp().getResponse();
+    const { response } = exception;
 
-        const status =
-            exception instanceof HttpException
-                ? exception.getStatus()
-                : HttpStatus.INTERNAL_SERVER_ERROR;
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
 
-        const reponseError = response.error;
+    if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
+      const error = exception.toString();
 
-        if (!reponseError) {
-            return responseOfRequest.status(status).send(response);
-        }
-
-        return responseOfRequest
-            .status(status)
-            .send(
-                new Result(response.message, false, null, [
-                    new Report('server', reponseError),
-                ]),
-            );
+      return responseOfRequest
+        .status(status)
+        .send(
+          new Result('Internal server error', false, null, [
+            new Report('server', error),
+          ]),
+        );
     }
+
+    const responseError = response.error;
+
+    if (!responseError) {
+      return responseOfRequest.status(status).send(response);
+    }
+
+    return responseOfRequest
+      .status(status)
+      .send(
+        new Result(response.message, false, null, [
+          new Report('server', responseError),
+        ]),
+      );
+  }
 }
